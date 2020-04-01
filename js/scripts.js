1749fbe085bf10989e0c84062e489f2bb7c36893
Christopher Huber <CHuber42.work@gmail.com>
2020-04-01 09:24:24 -0700

    refresh leap year js (good job chi)

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
+function leapYear(year){
+  if((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
 $(document).ready(function(){
-  
+  $("#leapYearForm").submit(function(event){
+   event.preventDefault();
+   var userInput = $("#inputYear").val();
+  var trueOrFalse = leapYear(userInput);
+   if (trueOrFalse === true){
+    $("#output").append("true");
+   } else {
+    $("#output").append("false");  
+   }
+  });
 
 });
